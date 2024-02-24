da8ee0a353f16c37deace4e6d3bd502d868202ba
defopp <defoplup@gmail.com>
2024-02-24 19:07:08 +0700

    apply auth form

--- front-end/category_catalog/app.js
+++ front-end/category_catalog/app.js
@@ -41,6 +41,130 @@ document.addEventListener("DOMContentLoaded", function () {
 // Сделал
 
 
+
+
+
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+const body = document.querySelector("body")
+// hide log div by click outside the auth_container
+function hideLogDiv(event) {
+    if (authcont.contains(event.target)) {
+        console.log('click in the div')
+    }
+    else {
+        console.log('click outside the div')
+        closeLogDiv(authdiv, regdiv, signindiv, authcont)
+        document.removeEventListener('click', hideLogDiv)
+    }
+}
+
+// close/open
+function closeLogDiv(authdiv, regdiv, signindiv,authcont){
+    authcont.style.transform = 'scale(0)'
+
+    setTimeout(function(){
+        authdiv.style.display = "none";
+        regdiv.style.display = "none";
+        signindiv.style.display = "none";
+        body.style.overflow = 'scroll'
+    }, 100)
+}
+
+function openSignIn(authdiv, regdiv, signindiv,authcont){
+    authdiv.style.display = "flex";
+    regdiv.style.display = "none";
+    signindiv.style.display = "flex";
+    
+    console.log('open sign in');
+
+    body.style.overflow = 'hidden';
+    
+    setTimeout(function(){
+        authcont.style.transform = 'scale(1)'
+    }, 100)
+    
+    setTimeout(function(){
+        document.addEventListener('click', hideLogDiv);
+    }, 1000)
+}
+
+function openSignUp(authdiv, regdiv, signindiv,authcont){
+    authdiv.style.display = "flex";
+    regdiv.style.display = "flex";
+    signindiv.style.display = "none";
+
+    console.log('open sign up');
+    
+    body.style.overflow = 'hidden';
+
+    setTimeout(function(){
+        authcont.style.transform = 'scale(1)'
+    }, 100)
+
+    setTimeout(function(){
+        document.addEventListener('click', hideLogDiv);
+    }, 1000)
+}
+
+// buttons and divs
+const SignUpButtons = document.querySelectorAll('#signup')
+const SignInButtons = document.querySelectorAll('#signin')
+
+const authdiv = document.querySelector('.authdiv')
+const authcont = document.querySelector('.auth_container')
+const regdiv = document.querySelector('.regdiv')
+const signindiv = document.querySelector('.signindiv')
+
+// buttons logic
+SignUpButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        openSignUp(authdiv, regdiv, signindiv, authcont);
+    })
+});    
+
+SignInButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        openSignIn(authdiv, regdiv, signindiv, authcont)
+    })
+});       
+
+
+
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+// log logic// log logic// log logic// log logic// log logic// log logic// log logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // title burger
 document.addEventListener('DOMContentLoaded', function (){
     let Tburger = document.getElementById('Tburger')
