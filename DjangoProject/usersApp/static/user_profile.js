baf680a195a203f53b9cdcae22253acd701c3f5c
defopp <defoplup@gmail.com>
2024-05-02 01:21:20 +0700

    js fixes

--- DjangoProject/usersApp/static/user_profile.js
+++ DjangoProject/usersApp/static/user_profile.js
@@ -1,4 +1,41 @@
-// слайдер 1 //сделать слайдер
+
+
+// IN PROGRESS
+// class Slider {
+
+//     constructor () {
+//         this.offset = 0;
+//     };
+
+//     start () {
+
+//         this.sliderLine = document.querySelector('.slider_items');
+//         document.querySelector(".next_sl_bt").addEventListener('click', function () {
+//             this.offset += 290;
+//             if (this.offset > 580) {
+//                 this.offset = 0
+//             }
+//             this.sliderLine.style.left = -this.offset + 'px';
+//         });
+
+//         document.querySelector(".prev_sl_bt").addEventListener('click', function () {
+//             this.offset -= 290;
+//             if (this.offset < 0) {
+//                 this.offset = 580
+//             }
+//             this.sliderLine.style.left = -this.offset + 'px';
+//         });
+
+//     }
+// }
+
+// const a = new Slider()
+// a.start()
+
+
+
+GARBAGE
+слайдер 1 //сделать слайдер
 let offset = 0;
 const sliderLine = document.querySelector('.slider_items')
 
@@ -17,3 +54,33 @@ document.querySelector(".prev_sl_bt").addEventListener('click', function () {
     }
     sliderLine.style.left = -offset + 'px';
 })
+
+
+// class Slider {
+//     constructor() {
+//         this.offset = 0;
+//     }
+
+//     start() {
+//         this.sliderLine = document.querySelector('.slider_items');
+
+//         document.querySelector(".next_sl_bt").addEventListener('click', () => {
+//             this.offset += 290;
+//             if (this.offset > 580) {
+//                 this.offset = 0;
+//             }
+//             this.sliderLine.style.left = -this.offset + 'px';
+//         });
+
+//         document.querySelector(".prev_sl_bt").addEventListener('click', () => {
+//             this.offset -= 290;
+//             if (this.offset < 0) {
+//                 this.offset = 580;
+//             }
+//             this.sliderLine.style.left = -this.offset + 'px';
+//         });
+//     }
+// }
+
+// const a = new Slider();
+// a.start();
