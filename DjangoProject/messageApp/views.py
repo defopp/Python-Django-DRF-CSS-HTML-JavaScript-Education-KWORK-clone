c3a7efa924c5637f8aa4c094cb953f2ab9dee693
defopp <defoplup@gmail.com>
2024-05-08 04:32:47 +0700

    EDIT readme.md

--- DjangoProject/messageApp/views.py
+++ DjangoProject/messageApp/views.py
@@ -6,10 +6,16 @@ from rest_framework import viewsets
 # Create your views here.
 class ChatView(View):
     template_name = 'messageApp\\template\\messenger.html'
+
     def get(self, request):
         return render(request, self.template_name)
     
-    
+
+
+
+
+
+
 # class MessageViewSet(viewsets.ModelViewSet):
 #     ...    
 
