1c395f18a1f7feee1fdf69b284d0b3254ad8339e
defopp <defoplup@gmail.com>
2024-05-12 12:22:02 +0700

    TEST create first ChatRoomDIV by JSON

--- DjangoProject/messageApp/static/messenger_chat.js
+++ DjangoProject/messageApp/static/messenger_chat.js
@@ -9,42 +9,78 @@
 // }
 
 
-// создание компонентов для страницы
-function getChatRoomDiv() {}
-
-function getDialogDiv() {}
-
-function getMessageDiv() {}
 
 
-
-// Ajax xRequests to API
+// 1. Ajax xRequests to API
 let url = `http://${window.location.host}/api/chatroom/`
 
 // XML METHOD
 let xhr = new XMLHttpRequest();
 xhr.open('get', url, true);
 xhr.onreadystatechange = function () {
-
     if (xhr.readyState == 4 && xhr.status == 200) {
         // console.log(xhr)
-        console.log(JSON.parse(xhr.responseText))
-    }
-
+        // console.log(JSON.parse(xhr.responseText))
+        const chatRoomsJson = JSON.parse(xhr.responseText)
+        createChatRoomsDivsByJSON(chatRoomsJson)
+    }    
 }
 xhr.send()
 
 
 
 
+// 2. Create ChatRoomDivs
+const chatRoomsDiv = document.getElementById('chatrooms')
+
+function createChatRoomsDivsByJSON(chatRoomsJson) {
+    chatRoomsJson.forEach(chatRoom => {
+        chatRoomsDiv.innerHTML += createChatRoomDiv(chatRoom) 
+    });  
+    alert('Все создал!')  
+};
 
 
-// FETCH request METHOD
-// let promise = fetch(url);
-// let response = new Response(promise)
-
-// if (response.ok) {
-//     console.log('ok ', response)
-// }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. создание компонентов для страницы
+function createChatRoomDiv(chatRoom) {
+    let chatRoomId = chatRoom.id
+    return `<div class="messege_div">
+                <div class="messege_div_title">
+                    <img class="messege_div_avatar" src='avatar_img'{% static "mainApp/img/medium.jpg" %} alt="avatar">
+                    <div class="user_info">
+                        <p>nickname</p>
+                        <p class="mes">message chatroom id - ${chatRoomId}</p>
+                    </div>  
+                    <div class="messege_info">
+                        <p class="date">date time</p>
+                    </div>
+                </div> 
+            </div>`
+        }
+
+
+
+
+
+
+
+
+
+function createDialogDiv() {}
+
+function createMessageDiv() {}
